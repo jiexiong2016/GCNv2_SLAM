9b11122825cd054c3d1510ad40eb3c410626e9e9
jiexiong2016 <jiexiong@kth.se>
2019-04-10 13:05:37 +0200

    Update run.sh

--- GCN2/run.sh
+++ GCN2/run.sh
@@ -1,3 +1,6 @@
+# GCNv2 trained with more augmentations and larger batch
+GCN_PATH=gcn2_aug.pt ./rgbd_gcn ../Vocabulary/GCNvoc.bin TUM3.yaml ~/Workspace/Datasets/TUM/freiburg3/rgbd_dataset_freiburg3_long_office_household ~/Workspace/Datasets/TUM/freiburg3/rgbd_dataset_freiburg3_long_office_household/associations.txt
+
 # GCNv2
 ./rgbd_gcn ../Vocabulary/GCNvoc.bin TUM3.yaml ~/Workspace/Datasets/TUM/freiburg3/rgbd_dataset_freiburg3_long_office_household ~/Workspace/Datasets/TUM/freiburg3/rgbd_dataset_freiburg3_long_office_household/associations.txt
 
