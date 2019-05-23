990327ad2397757d26a8bfa4aa8f07a90da079e9
Jiexiong Tang <jiexiong@kth.se>
2019-05-23 03:05:47 +0200

    Update run.sh

--- GCN2/run.sh
+++ GCN2/run.sh
@@ -1,4 +1,4 @@
-# Run GCN, removed resolution requirment. It may affect the actual performance. Ideally, GCNv2 should be trained/finetuned in desired resolution.
+# Run GCN, removed resolution requirment. The current model was trained with resolution 320x240 as input. Use other resolution may affect the actual performance. Ideally, GCNv2 should be trained/finetuned in desired resolution.
 GCN_PATH=gcn2.pt ./rgbd_gcn ../Vocabulary/GCNvoc.bin TUM3.yaml ~/Workspace/Datasets/TUM/freiburg3/rgbd_dataset_freiburg3_long_office_household ~/Workspace/Datasets/TUM/freiburg3/rgbd_dataset_freiburg3_long_office_household/associations.txt
 
 # Reproduce results in comparison with ORB as in our paper, will enable NN_ONLY and use 320x240 resolution.
